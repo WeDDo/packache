97f4cc3c4ab92ddcbcd84071280e0ad09b359db9
WeDDo <mantuxas001@gmail.com>
2021-10-02 01:14:10 +0300

    production env

---
+++ database/migrations/2021_09_25_222622_create_packages_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePackagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::create('packages', function (Blueprint $table) {
+            $table->id();
+            $table->integer('quantity')->unsigned();
+            $table->timestamps();
+            $table->unsignedBigInteger('item_id')->nullable();
+            $table->unsignedBigInteger('order_id')->nullable();
+        });
+
+        Schema::table('packages', function(Blueprint $table){
+            $table->foreign('item_id')
+            ->references('id')->on('items')
+            ->onDelete('cascade');
+
+            $table->foreign('order_id')
+            ->references('id')->on('orders')
+            ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('packages');
+    }
+}
